6b55957acb950a80e7e44d1eb6c3f62607faa77e
JC4github <95055206+JC4github@users.noreply.github.com>
2022-02-27 10:08:56 +0000

    Sun, Feb 27, 2022, 10:08 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,15 +4,18 @@
 
 int checkforvalid(int arr[], int length)
 {
-    int sum;
+    int sum1 = 0;
+    int sum2 = 0;
+
     for (int i = 1; i < length ; i+=2)
     {
-        sum = sum + (arr[i]*2);
+        sum1 = sum1 + (arr[i]*2); //dont add the products, but the digits
     }
     for (int i = 0; i < length ; i+=2)
     {
-        sum = sum + (arr[i]);
+        sum2 = sum2 + (arr[i]);
     }
+    int sum = sum1 + sum2;
     if (sum % 10 > 0)
     {
         return 2; //invalid
@@ -28,19 +31,21 @@ int main(void)
 {
     long long cardnum = get_long("Number: ");
 
-    int array[25];
-    while (cardnum > 0)
+    char numtext[256];
+    sprintf(numtext, "%lld", cardnum);
+    int size = strlen(numtext);
+
+    int array[size];
+    int i = 0;
+    long long num = cardnum;
+    while (num > 0)
     {
-        int num = cardnum;
-        int i = 0;
         array[i] = num % 10; //creates flipped array
         num = num / 10;
         ++i;
     }
 
-    char numtext[256];
-    sprintf(numtext, "%lld", cardnum);
-    int size = strlen(numtext);
+
 
     char cardtype[10];
 
@@ -51,7 +56,7 @@ int main(void)
     }
     else
     {
-        if (array[0] == 4)
+        if (array[size - 1] == 4)
         {
             if (checkforvalid(array, size) == 1)
             {
@@ -64,7 +69,7 @@ int main(void)
                 return printf("%s\n", cardtype);
             }
         }
-        else if (array[0] == 5 && array[1] > 0 && array[1] < 6)
+        else if (array[size - 1] == 5 && array[size - 2] > 0 && array[size - 2] < 6)
         {
             if (checkforvalid(array, size) == 1)
             {
@@ -77,7 +82,7 @@ int main(void)
                 return printf("%s\n", cardtype);
             }
         }
-        else if (array[0] == 3 && (array[1] == 4 || array[1] == 7))
+        else if (array[size - 1] == 3 && (array[size - 2] == 4 || array[size - 2] == 7))
         {
             if (checkforvalid(array, size) == 1)
             {
