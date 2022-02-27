dac9144904451ea092206d82cfc96d3ec7e00ae2
JC4github <95055206+JC4github@users.noreply.github.com>
2022-02-27 01:16:45 +0000

    Sun, Feb 27, 2022, 1:16 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,7 +6,16 @@ int main(void)
     long long cardnum = get_long("Number: ");
     char numtext[256];
     sprintf(numtext, "%lld", cardnum);
+
+    char cardtype[];
     if (numtext[0] == 4)
+    {
+        cardtype = "Visa"
+    }
+    else if (numtext[0] = 5)
+    {
+        cardtype = 
+    }
 
     printf("%s\n", numtext);
 }
