9af77e5d5f634d6efb498dc4f9ac115b6da0157f
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-01 23:57:02 +0000

    Tue, Mar 1, 2022, 11:57 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,32 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(void) 
+int main(void)
 {
+    string text = get_string("Text: ");
+    int length = strlen(text);
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
 
+    for (int i = 0; i < length; i++)
+    {
+        if ((text[i] >= 'A' || text[i] <= 'Z') || (text[i] >= 'a' || text[i] <= 'a'))
+        {
+            letters++;
+        }
+        else if(text[i] == ' ')
+        {
+            words++;
+        }
+        else if (text[i] == '!' || text[i] == '?' || text[i] == '.')
+        {
+            sentences++;
+        }
+    }
+
+    printf("%d %d %d", words, sentences, letters);
+    return 0;
 }
