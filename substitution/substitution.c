0507b75735efa020e526b57fb552d2ca4d8fd5c5
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-02 03:25:11 +0000

    Wed, Mar 2, 2022, 3:25 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string>
 
 int main(int argc, string argv[])
 {
-    
+    int length = strlen(argv);
+    if (length != 26)
+    {
+        printf("Key must contain 26 characters.");
+        return 1;
+    }
 }
