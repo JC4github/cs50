b41b0dc66b10ec76b1c97dfb063c4968b077fae9
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-08 20:54:35 +0000

    Tue, Mar 8, 2022, 8:54 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -214,15 +214,15 @@ void lock_pairs(void)
         int match = 0;
         for (int j = 0, j < pair_count; j++) //loop through seen j values
         {
-            if (pairs[i].loser != seen_j_values[j])
+            if (pairs[i].loser == seen_j_values[j])
             {
-                seen_j_values[i] = pairs[i].loser;
-                stillZero--;
+                match = 1; //seen it
             }
         }
         if (match != 1)
         {
-
+            seen_j_values[i] = pairs[i].loser;
+            stillZero--;
         }
         if (stillZero > 1)
         {
