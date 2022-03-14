1aab69a6a4f27f6224705a559e8667201076fb77
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-14 01:47:56 +0000

    Mon, Mar 14, 2022, 1:47 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -16,7 +16,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             red = image[i][j].rgbtRed;
             green = image[i][j].rgbtGreen;
             blue = image[i][j].rgbtBlue;
-            avg = round((red + blue + green)/3)
+            avg = round((red + blue + green) / 3);
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtBlue = avg;
         }
     }
     return;
