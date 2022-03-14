8de8a7fe226133651f00a7ada45961c5b07f6ea0
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-14 07:46:08 +0000

    Mon, Mar 14, 2022, 7:46 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -283,7 +283,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             }
         }
     }
-    image = tempimg;
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
