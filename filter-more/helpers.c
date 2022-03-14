14707c26be6820aa8a90d5b25e88278d628c80dc
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-14 21:27:15 +0000

    Mon, Mar 14, 2022, 9:27 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -569,12 +569,6 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                     int blue[9];
                     int green[9];
 
-                    s[0] = image[i][j - 1];
-                    s[1] = image[i - 1][j - 1];
-                    s[2] = image[i - 1][j];
-                    s[3] = image[i - 1][j + 1];
-                    s[4] = image[i][j + 1];
-                    s[5] = image[i][j];
                     s[0] = image[i - 1][j - 1];
                     s[1] = image[i - 1][j];
                     s[2] = image[i - 1][j + 1];
