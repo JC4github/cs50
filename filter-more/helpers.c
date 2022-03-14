7f9e062bd229ced8b82a6dd202283c25884dae1c
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-14 03:14:35 +0000

    Mon, Mar 14, 2022, 3:14 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -113,6 +113,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     s[0] = image[i][j + 1];
                     s[1] = image[i + 1][j + 1];
                     s[2] = image[i + 1][j];
+                    s[3] = image[i - 1][j];
+                    s[4] = image[i - 1][j + 1];
 
                     for (int k = 0; k < 3; k++)
                     {
