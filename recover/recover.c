d84c6b4280ba6f437289a5ce7eca877ff0941fef
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-15 02:08:43 +0000

    Tue, Mar 15, 2022, 2:08 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -26,6 +26,7 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, 1, 512, file) == 512)
     {
+        
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
         {
             if ((buffer[3] & 0xf0) == 0xe0)
