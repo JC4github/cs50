157ee2c7ffb6d4c65e38ffe0e54f45cb5c9c6411
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-15 01:40:23 +0000

    Tue, Mar 15, 2022, 1:40 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,6 +4,12 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
+if (argc > 2)
+{
+    
+}
+
+
     FILE *file = fopen(argv[1], "r");
 
     while (fread(buffer, 1, 512, raw_file) == 512)
