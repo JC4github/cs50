70eae111686f23cc861f1a577ed09964493cf55f
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-15 01:43:39 +0000

    Tue, Mar 15, 2022, 1:43 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,15 +4,20 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-if (argc > 2)
+if (argc != 2)
 {
-    
+    printf("only input one file");
+    return 1;
 }
 
-
     FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        printf("Cant open file");
+         return 1;
+    }
 
-    while (fread(buffer, 1, 512, raw_file) == 512)
+    while (fread(buffer, 1, 512, file) == 512)
     {
     }
 }
