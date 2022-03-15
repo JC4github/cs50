7b4e1fc754999f741bd9be602b5d906f82992fc0
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-15 02:04:46 +0000

    Tue, Mar 15, 2022, 2:04 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <cs50.h>
 
 typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-if (argc != 2)
-{
-    printf("only input one file");
-    return 1;
-}
+    if (argc != 2)
+    {
+        printf("only input one file");
+        return 1;
+    }
 
     FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
         printf("Cant open file");
-         return 1;
+        return 1;
     }
 
     int counter = 0;
     BYTE buffer[512];
+    string filename = NULL;
+
     while (fread(buffer, 1, 512, file) == 512)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
         {
             if ((buffer[3] & 0xf0) == 0xe0)
             {
-                sprintf
+                sprintf(filename, "%03i.jpg", counter);
+                FILE *img = fopen(filename, "w");
             }
         }
     }
