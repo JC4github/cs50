55ede022742f99d15ab04c6dc32b787098af20be
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-15 01:52:52 +0000

    Tue, Mar 15, 2022, 1:52 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -17,7 +17,12 @@ if (argc != 2)
          return 1;
     }
 
+    BYTE buffer[512];
     while (fread(buffer, 1, 512, file) == 512)
     {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
+        {
+            
+        }
     }
 }
