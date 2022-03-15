1612a7a32b748345f4f13afc0b4df03cf7201150
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-15 01:36:06 +0000

    Tue, Mar 15, 2022, 1:36 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+typedef uint8_t BYTE;
+
 int main(int argc, char *argv[])
 {
- 
+    FILE *file = fopen(argv[1], "r");
+
+    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+    {
+    }
 }
