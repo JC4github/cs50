9c9edb60506aad9b09fa585513f209dc20ab8d27
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-28 03:16:37 +0000

    Mon, Mar 28, 2022, 3:16 AM +00:00

--- sentimental-readability/readablility.py
+++ sentimental-readability/readablility.py
@@ -5,18 +5,18 @@ L = 0
 S = 0
 words = 1
 letters = 0
-sentence = 0
-for a in sentence:
+sentences = 0
+for letter in sentence:
     if (ord(letter) >= ord("A") and ord(letter) <= ord("Z")) or (ord(letter) >= ord("a") and ord(letter) <= ord("z")):
         letters += 1
     elif (ord(letter) == ord(" ")):
         words += 1
     elif ord(letter) == ord("!") or ord(letter) == ord(".") or ord(letter) == ord("?"):
-        sentence += 1
+        sentences += 1
 
 L = round((letters / words) * 100, 3)
-S = round((sentence / words) * 100, 3)
-index = round((0.0588 * L) - (0.296 * S) - 15.8, 3)
+S = round((sentences / words) * 100, 3)
+index = round((0.0588 * L) - (0.296 * S) - 15.8)
 
 if index < 1:
     print("Before Grade 1")
