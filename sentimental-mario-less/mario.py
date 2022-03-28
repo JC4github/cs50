08c784902a9c8acc3c315a659ea00c688e9422c4
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-28 01:06:17 +0000

    Mon, Mar 28, 2022, 1:06 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,5 +1,9 @@
 from cs50 import get_int
 
-input = get_int("Height: ")
-for i in range(input):
+height = get_int("Height: ")
+spaces = height - 1
+for i in range(height):
+    for space in range(spaces):
+        print(" ", end="")
+        spaces -= 1
     
