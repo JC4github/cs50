19b8c47519007505d0de5f2f045e0434c64329f5
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-28 01:09:18 +0000

    Mon, Mar 28, 2022, 1:09 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,8 +2,11 @@ from cs50 import get_int
 
 height = get_int("Height: ")
 spaces = height - 1
+blocks = 1
 for i in range(height):
     for space in range(spaces):
         print(" ", end="")
         spaces -= 1
-    
+    for block in range(blocks):
+        print("#", end="")
+        blocks += 1
