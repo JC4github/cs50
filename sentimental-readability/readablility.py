f7f3eccb5caac00b29e67c19977304e9abccae40
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-28 02:42:00 +0000

    Mon, Mar 28, 2022, 2:42 AM +00:00

--- sentimental-readability/readablility.py
+++ sentimental-readability/readablility.py
@@ -3,8 +3,13 @@ from cs50 import get_string
 sentence = get_string("Text: ")
 L = 0
 S = 0
+words = 1
+letters = 0
 for letter in sentence::
     if (ord(letter) >= ord("A") and ord(letter) <= ord("Z")) or (ord(letter) >= ord("a") and ord(letter) <= ord("z")):
-        L += 1
+        letters += 1
+    elif (ord(letter) == ord(" ")):
+        words += 1
+
 
 index = 0.0588 * L - 0.296 * S - 15.8
