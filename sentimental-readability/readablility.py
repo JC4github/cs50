8d01511a9ae2831413973d95da00ce6fd77e62fb
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-28 02:59:09 +0000

    Mon, Mar 28, 2022, 2:59 AM +00:00

--- sentimental-readability/readablility.py
+++ sentimental-readability/readablility.py
@@ -1,28 +1,31 @@
 from cs50 import get_string
 
-sentence = get_string("Text: ")
-L = 0
-S = 0
-words = 1
-letters = 0
-sentence = 0
-for letter in sentence:
-    if (ord(letter) >= ord("A") and ord(letter) <= ord("Z")) or (ord(letter) >= ord("a") and ord(letter) <= ord("z")):
-        letters += 1
-    elif (ord(letter) == ord(" ")):
-        words += 1
-    elif ord(letter) == ord("!") or ord(letter) == ord(".") or ord(letter) == ord("?"):
-        sentence += 1
+def main():
+    sentence = get_string("Text: ")
+    L = 0
+    S = 0
+    words = 1
+    letters = 0
+    sentence = 0
+    for letter in sentence:
+        if (ord(letter) >= ord("A") and ord(letter) <= ord("Z")) or (ord(letter) >= ord("a") and ord(letter) <= ord("z")):
+            letters += 1
+        elif (ord(letter) == ord(" ")):
+            words += 1
+        elif ord(letter) == ord("!") or ord(letter) == ord(".") or ord(letter) == ord("?"):
+            sentence += 1
 
-L = round((letters / words) * 100, 3)
-S = round((sentence / words) * 100, 3)
-index = round((0.0588 * L) - (0.296 * S) - 15.8, 3)
+    L = round((letters / words) * 100, 3)
+    S = round((sentence / words) * 100, 3)
+    index = round((0.0588 * L) - (0.296 * S) - 15.8, 3)
 
-if index < 1:
-    print("Before Grade 1")
+    if index < 1:
+        print("Before Grade 1")
 
-elif index > 16:
-    print("Grade 16+")
+    elif index > 16:
+        print("Grade 16+")
 
-else:
-    print(f"Grade {index}")
+    else:
+        print(f"Grade {index}")
+
+main()
