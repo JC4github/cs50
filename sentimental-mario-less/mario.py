275df7b8776d07330f4dc7d0b53c15ec2d4c00b7
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-28 01:15:40 +0000

    Mon, Mar 28, 2022, 1:15 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,6 +1,10 @@
 from cs50 import get_int
 
-height = get_int("Height: ")
+while True:
+    height = get_int("Height: ")
+    if height < 9 and height > 0:
+        break
+
 spaces = height - 1
 blocks = 1
 for i in range(height):
