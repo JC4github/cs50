d1c23235360ffb33d9bbef6d586b71a2ca7e0fcd
JC4github <95055206+JC4github@users.noreply.github.com>
2022-03-28 02:51:21 +0000

    Mon, Mar 28, 2022, 2:51 AM +00:00

--- sentimental-readability/readablility.py
+++ sentimental-readability/readablility.py
@@ -6,14 +6,16 @@ S = 0
 words = 1
 letters = 0
 sentence = 0
-for letter in sentence::
+for letter in sentence:
     if (ord(letter) >= ord("A") and ord(letter) <= ord("Z")) or (ord(letter) >= ord("a") and ord(letter) <= ord("z")):
         letters += 1
     elif (ord(letter) == ord(" ")):
         words += 1
-    elif (ord(letter) == ord("!") or (ord(letter) == ord(".")) or (ord(letter) == ord("?"):
+    elif ord(letter) == ord("!") or ord(letter) == ord(".") or ord(letter) == ord("?"):
         sentence += 1
 
-        
+L = round((letters / words) * 100, 3)
+S = round((sentence / words) * 100, 3)
+index = round((0.0588 * L) - (0.296 * S) - 15.8)
 
 index = 0.0588 * L - 0.296 * S - 15.8
