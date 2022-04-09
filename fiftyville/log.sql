5a5569f8d619c661a140535414fe603c846b5e8b
JC4github <95055206+JC4github@users.noreply.github.com>
2022-04-09 08:17:12 +0000

    Sat, Apr 9, 2022, 8:17 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,3 +3,6 @@
 SELECT description FROM crime_scene_reports WHERE year = 2021 AND day = 28;
 --found out theft took place at 10.15am, 3 witnesses mentions bakery
 
+SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28;
+--found all license plate on the day
+
