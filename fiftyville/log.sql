c57333562d4b3b7b26e3236ae316d7043211fd75
JC4github <95055206+JC4github@users.noreply.github.com>
2022-04-09 08:13:35 +0000

    Sat, Apr 9, 2022, 8:13 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-SELECT description FROM crime_scene_reports WHERE year = 2021, month = "July", day = 28;
+SELECT description FROM crime_scene_reports WHERE year = 2021 AND day = 28;
+--found out theft took place at 10.15am, 3 witnesses mentions bakery
+
