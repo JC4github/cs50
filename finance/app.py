0ce8ac9c3923420acca96ed816c026f57b4e1b8a
JC4github <95055206+JC4github@users.noreply.github.com>
2022-04-22 03:40:47 +0000

    Fri, Apr 22, 2022, 3:40 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -154,7 +154,10 @@ def quote():
             return apology("Must include a symbol")
         else:
             result = lookup(tickerSymbol)
-            return render_template("quoted.html", result=result)
+            if result == None:
+                return apology("Invalid symbol")
+            else:
+                return render_template("quoted.html", result=result)
     else:
         return render_template("quote.html")
 
