59705a8a3c281b6950bd692a3bfe365b5922a92f
JC4github <95055206+JC4github@users.noreply.github.com>
2022-04-22 02:00:42 +0000

    Fri, Apr 22, 2022, 2:00 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -77,7 +77,7 @@ def buy():
                 return apology("insufficient balance")
             else:
                 newBalance = balance[0]["cash"] - total
-                db.execute("INSERT INTO stocks (user_id, symbol, name, shares, price, total, date) VALUES(?, ?, ?, ?, ?, ?, ?)", session["user_id"], tickerSymbol, result["name"], shares, result["price"], total, date.today())
+                db.execute("INSERT INTO stocks (user_id, symbol, name, shares, price, total, action, date) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", session["user_id"], tickerSymbol, result["name"], shares, result["price"], total, ,"buy", date.today())
                 db.execute("UPDATE users SET cash = ? WHERE id = ?", newBalance, session["user_id"])
                 return redirect("/")
 
